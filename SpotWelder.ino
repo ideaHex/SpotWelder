c645f8ca620d1c5ee7edb77f596282702646e495
ideaHex <damian@ideahex.com>
2017-08-06 10:18:23 +0930

    Switches now working
    Small tidy up and now switches are working

--- SpotWelder.ino
+++ SpotWelder.ino
@@ -1,14 +1,29 @@
+/*  _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _
+   / \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \
+   \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/
+
+   Spot welder made from an old microwave
+
+     for more information visit
+      www.ideahex.com
+
+   Written by Damian Kleiss
+    _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _   _
+   / \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \
+   \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \*/
+
 /*
-To read Power switch
-enable pull up on switch pin shared with other 3 switches
-set switch wantinted to read to HIGH all otherd to LOW example below
+
+  To read Power switch
+  enable pull up on switch pin shared with other 3 switches
+  set switch wantinted to read to HIGH all otherd to LOW example below
 
   pinMode(15, OUTPUT);
   pinMode(16, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
 
-  digitalWrite(15,HIGH);  
+  digitalWrite(15,HIGH);
   digitalWrite(16,LOW);
   digitalWrite(12,LOW);
   digitalWrite(11,LOW);
@@ -27,67 +42,100 @@ set switch wantinted to read to HIGH all otherd to LOW example below
 
 #include <Encoder.h>
 
-#define DIGIT0  18 //1 Active High
-#define DIGIT1  19 //2
+///////////////// LED Display ///////////////////////
+#define DIGIT0  18  //1 Active High
+#define DIGIT1  19  //2
 #define DIGIT2  14
 #define DIGIT3  6
 #define DIGIT4  4
 #define NUM_DIGITS 5
 
-#define SEGMENTA    7 // 14 Active Low
-#define SEGMENTB    8// 13    
-#define SEGMENTC    9// 12  ALSO power switch
-#define SEGMENTD    10// 11
-#define SEGMENTE    11// 9
-#define SEGMENTF    16// 7  ALSO CLOCK SWITCH
-#define SEGMENTG    12// 8
-#define SEGMENT1    // 10
-#define SEGMENT2    // 
-
-#define SW_PWR_CLK          15
-#define SW_START_C1         SEGMENTF//16
-#define SW_GRL_WEI          SEGMENTG//12
-#define SW_STOP_C2          SEGMENTE//11
-#define SW_PWR_STOP_C1_GRL  SEGMENTB//8
-#define SW_START_C2_WEI_CLK SEGMENTF//7
-
-
-
-#define SWITCH_POWER1 9
-#define SWITCH_POWER2 15
-
-#define ENCODERA  2 // Use ext interupts
-#define ENCODERB  3 // Use ext interupts
-
-#define BUZZER 5
-
-#define FRED 1
-
-
-#define STATE_IDLE    0
-#define STATE_PREWELD 2
-#define STATE_WELD    3
+#define SEGMENTA    7   // 14 Active Low
+#define SEGMENTB    8   // 13    
+#define SEGMENTC    9   // 12  ALSO power switch
+#define SEGMENTD    10  // 11
+#define SEGMENTE    11  // 9
+#define SEGMENTF    16  // 7  ALSO CLOCK SWITCH
+#define SEGMENTG    12  // 8
+#define SEGMENT1        // 10
+#define SEGMENT2        // 
 
 #define BLANK 10
 #define COLON 1
 
+///////////////// Switch matrix ///////////////////////
+#define SW_PIN_PWR_CLK          15
+#define SW_PIN_START_C1         16//SEGMENTF//16
+#define SW_PIN_GRL_WEI          12//SEGMENTG//12
+#define SW_PIN_STOP_C2          11//SEGMENTE//11
+#define SW_PIN_PWR_STOP_C1_GRL  8//SEGMENTB//8
+#define SW_PIN_START_C2_WEI_CLK 7//SEGMENTF//7
+// Rows
+#define SW_PWR_CLK          0
+#define SW_START_C1         1
+#define SW_GRL_WEI          2
+#define SW_STOP_C2          3
+// Cols
+#define SW_PWR_STOP_C1_GRL  0
+#define SW_START_C2_WEI_CLK 1
+
+// SWITCHES
+#define SW_PWR    0
+#define SW_STOP   1
+#define SW_C1     2
+#define SW_GRL    3
+#define SW_START  4
+#define SW_C2     5
+#define SW_WEI    6
+#define SW_CLK    7
+
+#define NUM_SWITCHES 8
+#define NUM_ROWS  4
+#define NUM_COLS 2
+
+
+///////////////// ENCODER ///////////////////////
+#define ENCODERA  2 // Use ext interupts
+#define ENCODERB  3 // Use ext interupts
+
+///////////////// BUZZER ///////////////////////
+#define BUZZER 5
+
+#define FRED 1
+
+///////////////// State machine stuff ///////////////////////
+#define STATE_IDLE    0
+#define STATE_PREWELD 2
+#define STATE_DWELL   3
+#define STATE_WELD    4
+
+///////////////// Switch variables ///////////////////////
+bool switchState[NUM_SWITCHES];
+unsigned int switchRows[NUM_ROWS] = {SW_PIN_PWR_CLK, SW_PIN_START_C1, SW_PIN_GRL_WEI, SW_PIN_STOP_C2};
+unsigned int switchCols[NUM_COLS] = {SW_PIN_PWR_STOP_C1_GRL, SW_PIN_START_C2_WEI_CLK};
+
+///////////////// Display Variables ///////////////////////
 unsigned int period = 0;
 bool updateDisplay = 0;
 unsigned int displayHolder[NUM_DIGITS] = {BLANK, BLANK, BLANK, BLANK, BLANK};
+
 //                      zero      one       two         three   four      five      six         seven   eight     nine        blank
 const int numeral[] = {B1000000, B1111001, B0100100, B0110000, B0011001, B0010010, B0000010, B1111000, B0000000, B0010000, B1111111};//0b011000000, 0b110110010};
 int currentDigit = 1;
 
+///////////////// State machine variables ///////////////////////
 unsigned int state = STATE_IDLE;
 
-unsigned int preWeldTime = 0;
-unsigned int weldTime = 50;
+///////////////// Other variables ///////////////////////
 
-long oldPosition  = -999;
+// Weld times 
+// As the SSR is zero switching times will need to be in multiples of 1/100 HZ = 10 milliseconds
+unsigned int preWeldTime = 50; //milliseconds // 50ms is a good place to start
+unsigned int dwellTime = 20;    // Don't know
+unsigned int weldTime = 50;   // Between 50 and 250ms
 
-unsigned int powerSwitchState = 0;
-
-  long newPosition = 0;
+long encoderOldPosition  = -999;
+long encoderNewPosition = 0;
 
 // Change these two numbers to the pins connected to your encoder.
 //   Best Performance: both pins have interrupt capability
@@ -103,14 +151,13 @@ void setup()
   Serial.println("");
   Serial.println("Spot Welder by ideaHex");
   Serial.println("");
-  // initialize digital pin LED_BUILTIN as an output.
+
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(DIGIT0, OUTPUT);
   pinMode(DIGIT1, OUTPUT);
   pinMode(DIGIT2, OUTPUT);
   pinMode(DIGIT3, OUTPUT);
   pinMode(DIGIT4, OUTPUT);
-
 
   pinMode(SEGMENTA, OUTPUT);
   pinMode(SEGMENTB, OUTPUT);
@@ -120,28 +167,36 @@ void setup()
   pinMode(SEGMENTF, OUTPUT);
   pinMode(SEGMENTG, OUTPUT);
 
-  pinMode(SWITCH_POWER2, INPUT);
-  
-  //  pinMode(SEGMENT1, OUTPUT);
-  //  pinMode(SEGMENT2, OUTPUT);
+  pinMode(SW_PIN_PWR_CLK, OUTPUT);
 
-  //  digitalWrite(DIGIT1, HIGH);
 
   // Timer0 is already used for millis() - we'll just interrupt somewhere
   // in the middle and call the "Compare A" function below
   OCR0A = 0xAF;
   TIMSK0 |= _BV(OCIE0A);
-
-  pinMode(SWITCH_POWER2, INPUT_PULLUP);
-  attachInterrupt(0, SwitchPressed, FALLING);
 }
 
-void SwitchPressed()
+
+void UpdateSwitches()
 {
-  preWeldTime++;
-}
+  for (int row = 0; row < NUM_ROWS; row++)
+  {
+    // Set all rows low except the one we are wanting to check
+    for (int i = 0; i < NUM_ROWS; i++)
+    {
+      if (i == row) digitalWrite(switchRows[i], HIGH);
+      else  digitalWrite(switchRows[i], LOW);
+    }
+    pinMode(SW_PIN_START_C2_WEI_CLK, INPUT);
+    pinMode(SW_PIN_PWR_STOP_C1_GRL, INPUT);
+    switchState[2 * row] = digitalRead(SW_PIN_START_C2_WEI_CLK);
+    switchState[(2 * row) + 1] = digitalRead(SW_PIN_PWR_STOP_C1_GRL);
+    pinMode(SW_PIN_START_C2_WEI_CLK, OUTPUT);
+    pinMode(SW_PIN_PWR_STOP_C1_GRL, OUTPUT);
+  }
 
-// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+}
+// Interrupt is called once a millisecond
 SIGNAL(TIMER0_COMPA_vect)
 {
   if (period++ > FRED)
@@ -151,17 +206,7 @@ SIGNAL(TIMER0_COMPA_vect)
     writeNumeral(displayHolder[currentDigit]);
     if (currentDigit++ >= NUM_DIGITS)
     {
-      digitalWrite(SW_PWR_CLK,HIGH);  
-      digitalWrite(SW_START_C1,LOW); 
-      digitalWrite(SW_GRL_WEI,LOW);  
-      digitalWrite(SW_STOP_C2,LOW);  
-
-      pinMode(SW_PWR_STOP_C1_GRL,INPUT_PULLUP);
-      pinMode(SW_START_C2_WEI_CLK,OUTPUT);
-      digitalWrite(SW_START_C2_WEI_CLK,HIGH);
-
-      powerSwitchState = digitalRead(SW_PWR_STOP_C1_GRL);
-      pinMode(SW_PWR_STOP_C1_GRL,OUTPUT);
+      UpdateSwitches();
       currentDigit = 0;
     }
     updateDisplay = 0;
@@ -193,22 +238,22 @@ void writeNumeral(int num)
 
 void UpdateEncoder()
 {
-  newPosition = myEnc.read();
-  if (newPosition != oldPosition)
+  encoderNewPosition = myEnc.read();
+  if (encoderNewPosition != encoderOldPosition)
   {
-    if (newPosition > oldPosition) tone(BUZZER, 1200, 80);
+    if (encoderNewPosition > encoderOldPosition) tone(BUZZER, 1200, 80);
     else tone(BUZZER, 600, 80);
-    if (newPosition > 399)
+    if (encoderNewPosition > 399)
     {
       myEnc.write(399);
-      newPosition = 399;
+      encoderNewPosition = 399;
     }
-    if (newPosition < 0)
+    if (encoderNewPosition < 0)
     {
       myEnc.write(0);
-      newPosition = 0;
+      encoderNewPosition = 0;
     }
-    oldPosition = newPosition;
+    encoderOldPosition = encoderNewPosition;
     //Serial.println(newPosition);
   }
 }
@@ -223,7 +268,7 @@ void loop()
       if (displayHolder[4]  == COLON) displayHolder[4] = BLANK;
       else displayHolder[4] = COLON;
       //displayHolder[4]=8;
-      preWeldTime = newPosition / 4;
+      preWeldTime = encoderNewPosition / 4;
       if (preWeldTime / 10 < 1) displayHolder[0] = BLANK;
       else displayHolder[0] = preWeldTime / 10;
       displayHolder[1] = preWeldTime - ((preWeldTime / 10) * 10);
@@ -234,12 +279,24 @@ void loop()
       displayHolder[3] = weldTime - ((weldTime / 10) * 10);
       if (weldTime++ > 99) weldTime = 0;
 
-      Serial.println(powerSwitchState);
+      for (int j = 0; j < NUM_SWITCHES; j++)
+      {
+        Serial.print(switchState[j]);
+        Serial.print(",");
+      }
+      Serial.println("");
+      delay(200);
+
+      //Serial.println(powerSwitchState);
 
       //delay(200);
       break;
 
     case STATE_PREWELD:
+      Serial.println("Preweld");
+      break;
+
+    case STATE_DWELL:
       Serial.println("Preweld");
       break;
 
